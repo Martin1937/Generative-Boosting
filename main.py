16f0e2286256c7a9013303518b5b64a9d68ce740
Martin1937 <35216219+Martin1937@users.noreply.github.com>
2020-02-29 02:55:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from Sample_Data import gen
 from Generation_Training import cal
 
 if __name__ == '__main__':
-	for gap in range(1,26):
+	for gap in range(2,26):
 		window_size = 20
-		train_data,train_label,test_data,test_label,vali_data,vali_label = gen(window_size,gap,0)
+		MI_Cluster = 1
+		train_data,train_label,test_data,test_label,vali_data,vali_label,min_value,max_value = gen(window_size,gap,MI_Cluster)
+		feature = train_data.shape[2]
 		for average in range(0,30):
 			print("Window size: "+str(window_size)+" Time Lag is: "+str(gap))
-			test = cal(train_data,train_label,test_data,test_label,vali_data,vali_label,window_size,7,gap)
+			test = cal(train_data,train_label,test_data,test_label,vali_data,vali_label,window_size,feature,gap,min_value,max_value)
