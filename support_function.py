7d82ce1498e408df8f77f56929fedc0aa4881a27
Martin1937 <35216219+Martin1937@users.noreply.github.com>
2020-02-29 02:54:54 +0000

    Update support_function.py
    remove some redundant codes and improve running efficiency

--- support_function.py
+++ support_function.py
@@ -17,7 +17,7 @@ def MI_selection(all_data):
 			if sample_num_2 != sample_num:
 				P1 = all_data[all_data['Subject No']==sample_num].T
 				P2 = all_data[all_data['Subject No']==sample_num_2].T
-				MI = MI + mutinf(5,P1,P2)
+				MI = MI + mutinf(3,P1,P2)
 		all_MI.append(MI)
 	order = np.argsort(all_MI)[::-1]
 	new_order = original_number[order]
@@ -28,13 +28,17 @@ def MI_selection(all_data):
 #Random Split Data Patient at Patient Level
 #Input: DataFrame, Percentage of Training Data, Output:Splited Training Data & Test Data (in Numpy Form) 
 def random_split(all_data,percentage):
-
 	train_data_new = []
 	test_data_new = []
-
 	original_number = np.unique(all_data.values[:,0])
-	order = random.sample(range(len(original_number)-1),int(len(original_number)*percentage))
+	sampled_patient_index = np.random.choice(original_number,int(len(original_number)*percentage),replace=False)
+	unsampled_patient_index = np.array([i for i in original_number if i not in sampled_patient_index])
+	train_data_new = all_data[all_data['Subject No'].isin(sampled_patient_index)].values
+	test_data_new = all_data[all_data['Subject No'].isin(unsampled_patient_index)].values
+	return train_data_new, test_data_new
 
+'''
+	order = random.sample(range(len(original_number)-1),int(len(original_number)*percentage))
 	new_order = original_number[order]
 
 	for i in range(0,len(original_number)):
@@ -55,3 +59,4 @@ def random_split(all_data,percentage):
 				test_data_new = np.vstack((test_data_new,all_data[all_data['Subject No']==number]))							
 
 	return train_data_new, test_data_new
+'''
